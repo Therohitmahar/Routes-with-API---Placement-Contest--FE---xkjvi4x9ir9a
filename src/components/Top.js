1b7a5e1ff3003b1079a3d5c6700260d837760723
Rohit Mahar <113166435+Therohitmahar@users.noreply.github.com>
2023-05-27 16:22:09 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -6,25 +6,22 @@ function Top() {
   useEffect(() => {
     fetch("https://api.coinlore.net/api/tickers/?start=0&limit=10")
       .then((res) => res.json())
-      .then((data) => {
-        return setCryptoArr(data);
-      }, []);
-  });
+      .then((data) => setCryptoArr(data));
+  }, []);
   return (
     <div className="top-container">
       <h1>Top 10 Global Crypto Coins</h1>
       <div className="display-container">
-        {cryptoArr.length > 0 && (
-          <div className="display-container">
-            {cryptoArr.map((item) => (
+        <div className="display-container">
+          {cryptoArr.data &&
+            cryptoArr.data.map((item) => (
               <div key={item.id} className="coin-container">
                 <p className="rank-para">{item.rank}</p>
                 <h3 className="coin-symbol">{item.symbol}</h3>
                 <p className="price-para">{item.price_btc}</p>
               </div>
             ))}
-          </div>
-        )}
+        </div>
       </div>
     </div>
   );
